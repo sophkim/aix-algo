80869654a09119751a26d18c933556dca3f9b955
moongcheeze <jybyte@gmail.com>
2026-02-16 23:14:26 +0900

    🍎[정렬] sort(), lambda

---
+++ JY/_Baekjoon/11650.py
@@ -0,0 +1,13 @@
+# 좌표 정렬하기
+
+N = int(input())
+
+arr = []
+
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+
+arr.sort(key = lambda x : (x[0], x[1]))
+
+for i in range(N):
+    print(f"{arr[i][0]} {arr[i][1]}")
